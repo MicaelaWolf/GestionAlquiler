be314af2b5dfda09f99275412b332d940202eae9
Micaela Wolf <angimica99@gmail.com>
2023-08-03 19:38:30 -0300

    agregar persona

---
+++ src/views/persona/crear.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Crear Persona</title>
+  <link rel="stylesheet" href="public/css/persona/crear.css" type="text/css">
+</head>
+
+<body>
+  <form action="" method="post">
+    <input type="number" placeholder="CI" name="ci">
+    <input type="text" placeholder="Nombre" name="nombre">
+    <button type="reset">Borrar</button>
+    <input type="submit" value="Enviar">
+  </form>
+  <script src="public/js/persona/crear.js"></script>
+</body>
+
+</html>
